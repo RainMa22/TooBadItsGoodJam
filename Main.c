6aaaa1d2e146fa064deb27d3fa8fa49439092c08
felixrl <felix.liu.ray@gmail.com>
2024-06-30 20:44:51 -0600

    Added clickStats and init on switching scene

--- Main.c
+++ Main.c
@@ -2,6 +2,8 @@
 #include "button.h"
 #include "drawUtils.h"
 
+#include "clickStats.h"
+
 #define GAMENAME "BRAINROT TYCOON"
 #define TitleScene 0
 #define GameScene 1
@@ -11,16 +13,21 @@ int screenHeight = 600;
 //the collision box of mouse needs to have a size
 int mouseCollisionSize = 1;
 
+int switchTo(int);
+
+void TitleSceneInit(void);
 int TitleSceneProcedure(void);
+void GameSceneInit(void);
 int GameSceneProcedure(void);
 
 // main gameloop
 int main()
 {
-  int sceneID = 0;
+  int sceneID = switchTo(TitleScene);
 
   InitWindow(screenWidth, screenHeight, GAMENAME);
   SetTargetFPS(60);
+
   while (!WindowShouldClose())
   {
     switch (sceneID)
@@ -37,6 +44,27 @@ int main()
   }
   CloseWindow();
   return 0;
+}
+
+int switchTo(int sceneID) { // Calls the init function of the switch-to scene and then returns the sceneID.
+  switch (sceneID) {
+    case TitleScene:
+      TitleSceneInit();
+      break;
+    case GameScene:
+      GameSceneInit();
+      break;
+    default:
+      break;
+  }
+  return sceneID;
+}
+
+// -----------
+// TITLE SCENE
+// -----------
+
+void TitleSceneInit() {
 }
 
 // EFFECTS: draws the title scene with the Title text, a start button and a setting button
@@ -62,12 +90,35 @@ int TitleSceneProcedure()
   {
     if (CheckCollisionRecs(getButtonRect(startbtn),MouseRec))
     {
-      return GameScene;
+      return switchTo(GameScene);
     }
-    
   }
   EndDrawing();
   return TitleScene;
+}
+
+
+
+// ----------
+// GAME SCENE
+// ----------
+
+ClickStats clickStats;
+
+// Input logic
+bool leftClickPressed = false;
+
+void GameSceneInit() {
+  // printf("Initalising game scene...");
+  clickStats = newClickStats(); // A new click stats instance.
+  // TESTing click stats
+  // addClicks(&clickStats, 30);
+  // printf(clickStats.currentClicks);
+  // printf("remove 15");
+  // consumeClicks(&clickStats, 15);
+  // printf(clickStats.currentClicks);
+  // printf("lifetime");
+  // printf(clickStats.lifetimeClicks);
 }
 
 int GameSceneProcedure(){
@@ -77,6 +128,20 @@ int GameSceneProcedure(){
   const char *text = "This is the [Game Scene]\n\n(name pending)"; 
   DrawTextCentered(text,screenWidth/2,screenHeight/4,titleSize,DARKGRAY);
   //TODO
+
+  // Test for click stats
+  DrawTextCentered(TextFormat("Current clicks: %08i", clickStats.currentClicks),screenWidth/2,screenHeight/4 + 50,titleSize,DARKGRAY); // Print the current clicker count
+
+  if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+  {
+    if (!leftClickPressed) {
+      addClicks(&clickStats, 1); // Add clicks when mouse button just pressed (LMB)
+    }
+    leftClickPressed = true;
+  } else {
+    leftClickPressed = false;
+  }
+
   EndDrawing();
   return GameScene;
 }
---
+++ include/clickStats.h
@@ -0,0 +1,47 @@
+// CLICKER STYLES
+// This file defines the basic tracking and stats operations for the clicker game.
+
+// Guard
+#ifndef CLICK_STATS_H
+#define CLICK_STATS_H
+
+typedef struct 
+{
+    int lifetimeClicks; // Clicks accumulated thus far in this instance.
+    int currentClicks; // Clicks "currently avaliable" for use.
+} ClickStats;
+
+// Create a new click stats object and return it.
+ClickStats newClickStats(void) {
+    ClickStats stats = {0, 0};
+    return stats;
+}
+
+// Add a number of clicks.
+void addClicks(ClickStats* clickStatsPtr, int amount) {
+    (*clickStatsPtr).currentClicks += amount;
+    (*clickStatsPtr).lifetimeClicks += amount;
+}
+
+// Clamp the number of current clicks at zero.
+void clampCurrentClickCount(ClickStats* clickStatsPtr) {
+    if ((*clickStatsPtr).currentClicks < 0) {
+        (*clickStatsPtr).currentClicks = 0;
+    }
+}
+
+// Consume a number of clicks, but no negative values allowed in result!
+void consumeClicks(ClickStats* clickStatsPtr, int amount) {
+    (*clickStatsPtr).currentClicks -= amount;
+    clampCurrentClickCount(clickStatsPtr);
+}
+
+
+
+// Reset click counters to zero.
+void resetClicks(ClickStats* clickStatsPtr) {
+    (*clickStatsPtr).currentClicks = 0;
+    (*clickStatsPtr).lifetimeClicks = 0;
+}
+
+#endif CLICK_STATS_H
