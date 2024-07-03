cc685b3ed9bdbf25a19dfac03dd3f70e70ed5b1a
Rainma22 <d2rain@126.com>
2024-07-03 16:02:11 -0700

    fixed a bug where code doesn't compile when including stdio

--- include/dragbar.h
+++ include/dragbar.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "sceneDefinitions.h"
 #ifndef DRAGBAR_H
 #define DRAGBAR_H
@@ -10,59 +11,64 @@ typedef struct Dragbar
     Color color;
     Color selected;
     bool isSelected;
-}Dragbar;
+} Dragbar;
 
 // creates a new dragbar with the given bounds, buttonSize, percent, color, and selected color
-Dragbar newDragbar(Rectangle bounds, Vector2 buttonSize, char percent, Color color, Color selected){
-    int btnX = bounds.x+(bounds.width*percent-buttonSize.x/2)/100;
-    int btnY = bounds.y+(bounds.height-buttonSize.y)/2;
-    Button btn = newButton(btnX,btnY,buttonSize.x,buttonSize.y, 0,0,color,selected,"",RAYWHITE);
-    return (Dragbar){bounds,btn,percent,color,selected,false};
+Dragbar newDragbar(Rectangle bounds, Vector2 buttonSize, char percent, Color color, Color selected)
+{
+    int btnX = bounds.x + (bounds.width * percent - buttonSize.x / 2) / 100;
+    int btnY = bounds.y + (bounds.height - buttonSize.y) / 2;
+    Button btn = newButton(btnX, btnY, buttonSize.x, buttonSize.y, 0, 0, color, selected, "", RAYWHITE);
+    return (Dragbar){bounds, btn, percent, color, selected, false};
 }
 
 // update dragbar's percentage and button location with the given percentage
-Dragbar updateDragbarPercent(Dragbar self, char percent){
+Dragbar updateDragbarPercent(Dragbar self, char percent)
+{
     Button btn = self.dragButton;
     Rectangle bounds = self.bounds;
-    Rectangle buttonSize = (Rectangle){btn.sizeX,btn.sizeY};
-    
-    int btnX = bounds.x+(bounds.width*percent-buttonSize.x/2)/100;
-    int btnY = bounds.y+(bounds.height-buttonSize.y)/2;
-    
+    Rectangle buttonSize = (Rectangle){btn.sizeX, btn.sizeY};
+
+    int btnX = bounds.x + (bounds.width * percent - buttonSize.x / 2) / 100;
+    int btnY = bounds.y + (bounds.height - buttonSize.y) / 2;
+
     btn.x = btnX;
     btn.y = btnY;
     self.dragButton = btn;
     self.percent = percent;
 }
 
-void updateDragbar(Dragbar self){
+void updateDragbar(Dragbar self)
+{
     Rectangle mouseRect = getMouseRect();
     Rectangle buttonRect = getButtonRect(self.dragButton);
-    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && CheckCollisionRecs(mouseRect,buttonRect))
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && CheckCollisionRecs(mouseRect, buttonRect))
     {
         self.isSelected = true;
-    } else if (IsMouseButtonUp(MOUSE_BUTTON_LEFT)){
+    }
+    else if (IsMouseButtonUp(MOUSE_BUTTON_LEFT))
+    {
         self.isSelected = false;
     }
 
     if (self.isSelected)
     {
         int deltaX = mouseRect.x - self.bounds.x;
-        char newPercent = roundf(Clamp((float)deltaX - (float)self.bounds.width, 0, 100)); 
+        char newPercent = Clamp((float)deltaX - (float)self.bounds.width, 0, 100);
         updateDragbarPercent(self, newPercent);
     }
 }
 
-void drawDragbar(Dragbar bar){
-    DrawLine(bar.bounds.x,(bar.bounds.y+bar.bounds.height)/2, bar.bounds.x+bar.bounds.width, (bar.bounds.y+bar.bounds.height)/2, bar.color);
+void drawDragbar(Dragbar bar)
+{
+    DrawLine(bar.bounds.x, (bar.bounds.y + bar.bounds.height) / 2, bar.bounds.x + bar.bounds.width, (bar.bounds.y + bar.bounds.height) / 2, bar.color);
     drawButton(bar.dragButton);
+    updateDragbar(bar);
 }
 
-
-
-char getPercentage(Dragbar bar){
+char getPercentage(Dragbar bar)
+{
     return bar.percent;
 }
 
-
-#endif //DRAGBAR_H
+#endif // DRAGBAR_H
--- include/settingsBox.h
+++ include/settingsBox.h
@@ -1,5 +1,5 @@
-#include "dragbar.h"
 #include "drawUtils.h"
+#include "dragbar.h"
 
 #ifndef SETTINGSBOX_H
 #define SETTINGSBOX_H
@@ -19,15 +19,15 @@ SettingsBox newSettingsBox(Rectangle bounds, int thickness, Color lineColor)
 {
     Vector2 selfPos = (Vector2){bounds.x, bounds.y};
     Vector2 volumePos = relativeToAbsolute(selfPos, (Vector2){bounds.width / 8, 0});
-    Dragbar volume = newDragbar((Rectangle){volumePos.x, volumePos.y, bounds.width * 6 / 8, bounds.height / 5},
+    Dragbar volume = newDragbar((Rectangle){volumePos.x, volumePos.y, bounds.width * 4 / 8, bounds.height / 5},
                                 (Vector2){bounds.width / 16, bounds.height / 6}, 100, DARKGRAY, GRAY);
 
     Vector2 narrationPos = relativeToAbsolute(selfPos, (Vector2){bounds.width / 8, bounds.height / 5});
-    Dragbar narration = newDragbar((Rectangle){volumePos.x, volumePos.y, bounds.width * 6 / 8, bounds.height / 5},
+    Dragbar narration = newDragbar((Rectangle){narrationPos.x, narrationPos.y, bounds.width * 4 / 8, bounds.height / 5},
                                    (Vector2){bounds.width / 16, bounds.height / 6}, 100, DARKGRAY, GRAY);
 
     Vector2 yapVolPos = relativeToAbsolute(selfPos, (Vector2){bounds.width / 8, bounds.height * 2 / 5});
-    Dragbar yapVol = newDragbar((Rectangle){volumePos.x, volumePos.y, bounds.width * 6 / 8, bounds.height / 5},
+    Dragbar yapVol = newDragbar((Rectangle){yapVolPos.x, yapVolPos.y, bounds.width * 4 / 8, bounds.height / 5},
                                 (Vector2){bounds.width / 16, bounds.height / 6}, 100, DARKGRAY, GRAY);
 
     return (SettingsBox){bounds, thickness, lineColor, volume, narration, yapVol};
